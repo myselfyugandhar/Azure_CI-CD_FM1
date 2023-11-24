84ebba2966d4dfb7426a8635d02f303adf056087
Yugandhar V <100063501+myselfyugandhar@users.noreply.github.com>
2023-11-24 07:36:09 +0530

    Update dummy1.java

--- src/test/java/dummy/dummy1.java
+++ src/test/java/dummy/dummy1.java
@@ -1,7 +1,7 @@
 package dummy;
 
-import org.openqa.selenium.By;
-// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 // import org.openqa.selenium.chrome.ChromeDriver;
 
 public class dummy1 {
