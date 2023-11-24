8f9d974692c8003479fe5c7d1ffb8e1fb4e37e93
myselfyugandhar <yugandharvakkalakula@gmail.com>
2023-11-24 07:02:07 +0530

    push

---
+++ src/test/java/dummy/dummy1.java
@@ -0,0 +1,17 @@
+package dummy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class dummy1 {
+	public static void main(String args[]) throws InterruptedException
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.geeksforgeeks.org/");
+        Thread.sleep(5000);
+        String text = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[1]/div[1]/div[1]")).getText();
+        System.out.println(text);
+    }
+}
