48affc4273ad088e297e45a06c67724a9b09b7ea
Yugandhar V <100063501+myselfyugandhar@users.noreply.github.com>
2023-11-24 07:29:46 +0530

    Update dummy1.java

--- src/test/java/dummy/dummy1.java
+++ src/test/java/dummy/dummy1.java
@@ -1,8 +1,8 @@
 package dummy;
 
-// import org.openqa.selenium.By;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class dummy1 {
 	public static void main(String args[]) throws InterruptedException
