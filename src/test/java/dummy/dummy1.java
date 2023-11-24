7986355a0115b4ad693f5d4aa0e7e51647579059
Yugandhar V <100063501+myselfyugandhar@users.noreply.github.com>
2023-11-24 07:16:24 +0530

    Update dummy1.java

--- src/test/java/dummy/dummy1.java
+++ src/test/java/dummy/dummy1.java
@@ -7,11 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class dummy1 {
 	public static void main(String args[]) throws InterruptedException
     {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.geeksforgeeks.org/");
-        Thread.sleep(5000);
-        String text = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[1]/div[1]/div[1]")).getText();
-        System.out.println(text);
+        // WebDriver driver = new ChromeDriver();
+        // driver.manage().window().maximize();
+        // driver.get("https://www.geeksforgeeks.org/");
+        // Thread.sleep(5000);
+        // String text = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[1]/div[1]/div[1]")).getText();
+        // System.out.println(text);
     }
 }
