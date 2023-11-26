681396242ed542375ff451e509cca24e79f4fb1c
Yugandhar V <100063501+myselfyugandhar@users.noreply.github.com>
2023-11-26 08:34:54 +0530

    Update dummy1.java

--- src/test/java/dummy/dummy1.java
+++ src/test/java/dummy/dummy1.java
@@ -24,7 +24,7 @@ public class dummy1 {
         driver.manage().window().maximize();
         driver.get("https://www.geeksforgeeks.org/");
         Thread.sleep(5000);
-        String text = driver.findElement(By.xpath("/htmlbody/div[2]/div/div/div/div[1]/div[1]/div[1]")).getText();
+        String text = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[1]/div[1]/div[1]")).getText();
         System.out.println(text);
         driver.quit();
     }
